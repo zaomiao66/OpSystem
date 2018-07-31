c0cdb4e67f94095269c3063d9d828e0bfbc6c89b
zaomiao66 <296282091@qq.com>
2018-07-31 20:33:17 +0800

    0713_4

--- src/reducers/messageState.js
+++ src/reducers/messageState.js
@@ -1,20 +1,5 @@
 import * as ActionTypes from '../const/ActionTypes';
-const initState = {
 
-        nick: '快乐水！',
-        id: 'MID6330900002',
-        history_pay: '666',
-        tel: '16666666666',
-        learningLesson: ["摄影课","绘画课"],
-        totalLearningDays: '666天',
-        weiChatCode: 'zaomiao66 ',
-        enterDate: '2016-06-06',
-        lastLoginDate: '2016-06-06',
-        remark: '输入备注',
-        hurl:'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-
-
-};
 
 //纯函数reducer 接收旧的 state 和 action，返回新的 state。
 export default function menu(state = {  }, action){
--- src/reducers/switchState.js
+++ src/reducers/switchState.js
@@ -2,67 +2,7 @@
 import * as ActionTypes from '../const/ActionTypes';
 import React from 'react'
 import './switchState.css'
-const initState = {
 
-
-    historyLessonsList : [{
-        key: '1',
-        classInfo: {id:98858,
-                     name:"高级班"},
-        status: 1,
-        startTime: '2017-04-30',
-        enterRate:'21/21',
-        homeworkSubmitRate:'80%',
-        beCommenttedRate:'100%',
-        signRate:'5/21',
-        satisfyRate:'90%',
-        teacherInfo:{
-            id:76544,
-            mid:"98676",
-            nick:"小白老师",
-            realName:"白帆",
-            wxCode:"fgg"
-        }
-    }],
-
-    currentLessonsList : [{
-        key: '1',
-        classInfo: {id:98858,
-            name:"高级班"},
-        status: 1,
-        startTime: '2017-04-30',
-        enterRate:'21/21',
-        homeworkSubmitRate:'80%',
-        beCommenttedRate:'100%',
-        signRate:'5/21',
-        satisfyRate:'90%',
-        teacherInfo:{
-            id:76544,
-            mid:"98676",
-            nick:"小白老师",
-            realName:"白帆",
-            wxCode:"fgg"
-        }
-    }, {
-        key: '2',
-        classInfo: {id:98858,
-            name:"高级班"},
-        status: 1,
-        startTime: '2017-04-30',
-        enterRate:'21/21',
-        homeworkSubmitRate:'80%',
-        beCommenttedRate:'100%',
-        signRate:'5/21',
-        satisfyRate:'90%',
-        teacherInfo:{
-            id:76544,
-            mid:"98676",
-            nick:"小白老师",
-            realName:"白帆",
-            wxCode:"fgg"
-        }
-    }]
-};
 
 export default function todoList(state = [], action) {
     switch (action.type) {
